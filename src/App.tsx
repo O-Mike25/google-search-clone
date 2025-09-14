09c7253fec720eccb2b0b2034fa4faa4d8bf4e45
O-Mike25 <odjomike25@gmail.com>
2025-09-14 15:38:21 +0100

    feat(ui) : add basics navbar, color mode button and some links

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from "react";
+
+import Navbar from  "./components/Navbar";
+import Footer from  "./components/Footer";
+import AppRoutes from "./components/AppRoutes";
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div>App</div>
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+        <AppRoutes/>
+        <Footer/>
+      </div>
+    </div>
   )
 }
-
 export default App
